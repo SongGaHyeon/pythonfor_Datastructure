b48ab8a5d412edf8982c061faa8998fc15be2279
serasong <gahyunsong@hufs.ac.kr>
2022-02-10 22:18:50 +0900

    백준

---
+++ 백준/11725.py
@@ -0,0 +1,23 @@
+n= int(input())
+
+tree=[[] for i in range(n+1)]
+
+for i in range(n-1):
+    a,b=list(map(int,input().split()))
+    tree[a].append(b)
+    tree[b].append(a)
+
+q=[1]
+asw={}
+check_again=[False for i in range(n+1)]
+
+while len(q)>0:
+    parent=q.pop(0)
+    for i in tree[parent]:
+        if not check_again[i]:
+            asw[i]=parent
+            q.append(i)
+            check_again[i]=True
+
+for i in range(2,n+1):
+    print(asw[i])
