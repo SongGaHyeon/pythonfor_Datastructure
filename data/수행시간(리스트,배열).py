823b31f10f84859cc445974e870c7f9492a85899
serasong <gahyunsong@hufs.ac.kr>
2022-02-17 17:01:53 +0900

    백준

---
+++ data/수행시간(리스트,배열).py
@@ -0,0 +1,20 @@
+# 1. A[i]= A[j]+1
+# O(1)시간
+
+# 2. A.append(5)
+# O(1)시간
+
+# 3. A.pop()
+# O(1)시간
+
+# 4.A.pop(3)
+# O(len(A))시간
+
+# 5.A.insert(0,10)
+# O(len(A))시간
+
+# 6.A.remove(9)
+# O(n) 시간 -9를 찾아야함
+
+# 7.A.index(9), A.count(9)
+# O(len(A))시간!
